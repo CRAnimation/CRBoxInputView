3b0390623d3826485c1d62f300afd9f1bd975b59
BearXR <648070256@qq.com>
2019-06-11 12:18:15 +0800

    调整注释

--- Example/CRBoxInputView/CRBoxInputView/CRBoxInputView.h
+++ Example/CRBoxInputView/CRBoxInputView/CRBoxInputView.h
@@ -12,35 +12,41 @@
 #import "CRBoxInputCell.h"
 @class CRBoxInputView;
 
-typedef void(^TextDidChangeblock)(NSString *text, BOOL isFinished);
+typedef void(^TextDidChangeblock)(NSString * _Nullable text, BOOL isFinished);
 
 @interface CRBoxInputView : UIView
 
 /**
+ 是否需要光标
  ifNeedCursor
- *default: YES
+ default: YES
  */
 @property (assign, nonatomic) BOOL ifNeedCursor;
 
 /**
+ 验证码长度
  codeLength
  default: 4
  */
 @property (nonatomic, assign) NSInteger codeLength;
 
 /**
+ 是否开启密文模式
  ifNeedSecurity
  default: NO
  */
 @property (assign, nonatomic) BOOL ifNeedSecurity;
 
 /**
- show security delay time
+ 显示密文的延时时间
+ securityDelay
+ desc: show security delay time
  default: 0.3
  */
 @property (assign, nonatomic) CGFloat securityDelay;
 
 /**
+ 键盘类型
  keyBoardType
  default: UIKeyboardTypeNumberPad
  */
@@ -48,6 +54,7 @@ typedef void(^TextDidChangeblock)(NSString *text, BOOL isFinished);
 
 /**
  textContentType
+ 描述: 你可以设置为 'nil' 或者 'UITextContentTypeOneTimeCode' 来自动获取短信验证码
  desc: You set this 'nil' or 'UITextContentTypeOneTimeCode' to auto fill verify code.
  default: nil
  */
@@ -58,23 +65,36 @@ typedef void(^TextDidChangeblock)(NSString *text, BOOL isFinished);
 @property (strong, nonatomic) CRBoxInputCellProperty * _Nullable customCellProperty;
 @property (strong, nonatomic, readonly) NSString  * _Nullable textValue;
 
-// loadAndPrepareView is default beginEdit to YES.
+/**
+ 装载数据和准备界面
+ desc: Load and prepareView
+ beginEdit: 自动开启编辑模式
+ default: YES
+ */
 - (void)loadAndPrepareView;
 - (void)loadAndPrepareViewWithBeginEdit:(BOOL)beginEdit;
 
-// clearAll is default beginEdit to YES.
+/**
+ 清空输入
+ desc: Clear all
+ beginEdit: 自动开启编辑模式
+ default: YES
+ */
 - (void)clearAll;
 - (void)clearAllWithBeginEdit:(BOOL)beginEdit;
 
 - (UICollectionView *_Nullable)mainCollectionView;
 
+// 快速设置
 // Qiuck set
 - (void)quickSetSecuritySymbol:(NSString *_Nullable)securitySymbol;
 
+// 你可以在继承的子类中调用父类方法
 // You can inherit and call super
 - (void)initDefaultValue;
 
+// 你可以在继承的子类中重写父类方法
 // You can inherit and rewrite
-- (UICollectionViewCell *)customCollectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *_Nullable)indexPath;
+- (UICollectionViewCell *_Nullable)customCollectionView:(UICollectionView *_Nullable)collectionView cellForItemAtIndexPath:(NSIndexPath *_Nullable)indexPath;
 
 @end
