41a0b2a29071867e6189ead5145a2618e0e0e821
BearXR <648070256@qq.com>
2019-06-11 12:09:18 +0800

    增加说明

--- Example/CRBoxInputView/CRBoxInputView/CRBoxInputCellProperty.h
+++ Example/CRBoxInputView/CRBoxInputView/CRBoxInputCellProperty.h
@@ -23,7 +23,6 @@ typedef void(^ConfigCellShadowBlock)(CALayer *layer);
 @interface CRBoxInputCellProperty : NSObject <NSCopying>
 
 #pragma mark - UI
-
 /**
  cell边框宽度
  默认：0.5
@@ -97,23 +96,79 @@ typedef void(^ConfigCellShadowBlock)(CALayer *layer);
  */
 @property (assign, nonatomic) CGFloat cornerRadius;
 
-#pragma mark - label
-@property (copy, nonatomic) UIFont *cellFont;
-@property (copy, nonatomic) UIColor *cellTextColor;
+
 
 #pragma mark - line
+/**
+ 显示下划线
+ 默认： NO
+ */
 @property (assign, nonatomic) BOOL showLine;
 
+
+
+#pragma mark - label
+/**
+ 字体/字号
+ 默认：[UIFont systemFontOfSize:20];
+ */
+@property (copy, nonatomic) UIFont *cellFont;
+
+/**
+ 字体颜色
+ 默认：[UIColor blackColor];
+ */
+@property (copy, nonatomic) UIColor *cellTextColor;
+
+
+
 #pragma mark - Security
+/**
+ 是否密文显示
+ 默认：NO
+ */
 @property (assign, nonatomic) BOOL ifShowSecurity;
+
+/**
+ 密文符号
+ 默认：✱
+ 说明：只有ifShowSecurity=YES时，有效
+ */
 @property (copy, nonatomic) NSString *securitySymbol;
+
+/**
+ 默认填充值
+ 默认：空
+ 说明：在输入框没有内容时，会显示该值。
+ */
 @property (copy, nonatomic) NSString *originValue;
+
+/**
+ 密文类型
+ 默认：CRBoxSecuritySymbolType
+ 类型说明：
+ CRBoxSecuritySymbolType 符号类型，根据securitySymbol，originValue的内容来显示
+ CRBoxSecurityCustomViewType 自定义View类型，可以自定义密文状态下的图片，View
+ */
 @property (assign, nonatomic) CRBoxSecurityType securityType;
 
+
+
 #pragma mark - Block
+/**
+ 自定义密文View回调
+ */
 @property (copy, nonatomic) CustomSecurityViewBlock customSecurityViewBlock;
+/**
+ 自定义下划线回调
+ */
 @property (copy, nonatomic) CustomLineViewBlock customLineViewBlock;
+/**
+ 自定义阴影回调
+ */
 @property (copy, nonatomic) ConfigCellShadowBlock __nullable configCellShadowBlock;
+
+
 
 #pragma mark - Test
 @property (assign, nonatomic) NSInteger index;
